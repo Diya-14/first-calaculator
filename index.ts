344a024f756c3d874ec3c4fe36c7cd4745c5a5a6
Diya-14 <diyaiqbal346@gmail.com>
2024-03-24 07:45:06 +0500

    add some files

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node //SHABANG
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "enter first number", type: "number", name: "firstNumber" },
+  { message: "enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "select one of the operators to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Mutiplication","Division"],
+  },
+]);
+
+//conditional statement
+if(answer.operator ==="Addition"){
+    console.log(answer.firstNumber + answer.secondNumber);
+} else if(answer.operator ==="Subtraction"){
+    console.log(answer.firstNumber - answer.secondNumber);
+}else if(answer.operator ==="Multiplication")
+{console.log(answer.firstNumber * answer.secondNumber);
+}else if(answer.operator ==="Division"){
+    console.log(answer.firstNumber / answer.secondNumber);
+}else{
+    console.log("please select valid operator")
+}
